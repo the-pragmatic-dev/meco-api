2cf1cf43ccfb9fe1129580db80b1805a05b0ed18
Stephen Cathcart <steve@thepragmaticdev.uk>
2020-04-16 12:06:01 +0100

    fixing ipv4cidr validator

--- src/main/java/uk/thepragmaticdev/kms/validation/Ipv4Cidr.java
+++ src/main/java/uk/thepragmaticdev/kms/validation/Ipv4Cidr.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import javax.validation.Constraint;
+import javax.validation.Payload;
 
 @Constraint(validatedBy = Ipv4CidrValidator.class)
 @Retention(RetentionPolicy.RUNTIME)
@@ -17,4 +18,19 @@ public @interface Ipv4Cidr {
    * @return The error message if validation fails
    */
   String message() default "Must match n.n.n.n/m where n=1-3 decimal digits, m = 1-3 decimal digits in range 1-32.";
+
+  /**
+   * Specifies the processing groups with which the constraint declaration is
+   * associated.
+   * 
+   * @return The default empty array value
+   */
+  Class<?>[] groups() default {};
+
+  /**
+   * The payload with which the constraint declaration is associated.
+   * 
+   * @return The default empty array value
+   */
+  Class<? extends Payload>[] payload() default {};
 }
